d01a3dc19e99905a5e98ad1451c3a01e623e818a
Serg-o-code <sergeysamokhvalov97@gmail.com>
2025-04-17 18:20:32 +0500

    Добавлена заставка перед запуском основного приложения

--- my_clients.py
+++ my_clients.py
@@ -1,5 +1,6 @@
 import os
-from tkinter import Tk
+from splash_screen import show_splash_screen
+import tkinter as tk
 
 
 class MainApp:
@@ -7,15 +8,34 @@ class MainApp:
     def __init__(self, root):
         self.root = root
         self.root.title("My clients")
-        
-        # Установка иконки приложения
-        icon_path = os.path.join(os.path.dirname(
-            __file__), "resources", "icon.ico")
-        self.root.iconbitmap(icon_path)
+        self.root.geometry("500x500+500+150")
 
+        # Установка иконки приложения
+        self.set_icon()
+        
+        # Отрисовка интерфейса
+        self.render_ui()
+
+    def set_icon(self):
+        # Метод для установки иконки виджета
+        icon_path = os.path.join(
+            os.path.dirname(__file__),
+            "resources",
+            "icon.ico"
+        )
+        if os.path.exists(icon_path):
+            self.root.iconbitmap(icon_path)
+
+    def render_ui(self):
+        # Скоро здесь будут методы для отрисовки интерфейса
+        pass
+
+def start_main_app(splash):
+    splash.destroy()
+    root = tk.Tk()
+    app = MainApp(root)
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    root = Tk()
-    app = MainApp(root)
-    root.mainloop()
+    show_splash_screen(start_main_app, "Добро пожаловать", "Дарья")
---
+++ splash_screen.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from typing import Callable
+
+
+def show_splash_screen(
+    start_main_widget: Callable,
+    text: str = "Добро пожаловать",
+    name: str = "пользователь"
+) -> None:
+    # Функция для создания заставки
+    splash = tk.Tk()
+    splash.overrideredirect(True)  # Убрать рамки окна
+    splash.geometry("400x250+600+250")  # Размер окна + расположение на экране
+
+    splash.configure(bg="#1e1e1e")  # Фоновый цвет
+
+    label = tk.Label(splash, text=f"{text}, {name}!", font=(
+        "Arial", 14), bg="#1e1e1e", fg="white")  # Настройка приветствия
+    label.pack(expand=True)  # Размещение посередине экрана
+
+    # После окончания заставки будет запущен основной цикл
+    try:
+        splash.after(2500, lambda: start_main_widget(splash))
+        splash.mainloop()
+    except Exception as e:
+        print(f"Ошибка запуска главного окна: {e}")
